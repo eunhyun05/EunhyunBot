41349cf488a82d897ef1707d8cd4d2148882cab8
eunhyun05 <eunhyun050812@gmail.com>
2024-08-23 13:56:10 +0900

    feat: add HelpCommand for simple commands

---
+++ main/src/main/java/io/github/eunhyun/eunhyunbot/bot/command/simple/HelpCommand.java
@@ -0,0 +1,46 @@
+package io.github.eunhyun.eunhyunbot.bot.command.simple;
+
+import io.github.eunhyun.eunhyunbot.api.bot.command.simple.ISimpleCommand;
+import io.github.eunhyun.eunhyunbot.api.bot.command.simple.SimpleCommand;
+import io.github.eunhyun.eunhyunbot.api.factory.EmbedColorFactory;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import org.reflections.Reflections;
+
+import java.awt.*;
+import java.util.Set;
+
+@SimpleCommand(
+        command = "도움말",
+        description = "물음표 명령어 사용 방법을 출력합니다.",
+        usage = "?도움말"
+)
+public class HelpCommand implements ISimpleCommand {
+
+    private final Color EMBED_COLOR = EmbedColorFactory.getEmbedColor(EmbedColorFactory.Type.NORMAL);
+
+    @Override
+    public void execute(MessageReceivedEvent event) {
+        event.getMessage().delete().queue();
+
+        EmbedBuilder embed = new EmbedBuilder()
+                .setColor(EMBED_COLOR)
+                .setTitle("<a:foxhuhquestion:1276380174534967357> 도움말 | 물음표 명령어 <a:foxhuhquestion:1276380174534967357>");
+
+        String packageName = "io.github.eunhyun.eunhyunbot.bot.command.simple";
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(SimpleCommand.class);
+
+        embed.addBlankField(false);
+
+        annotated.forEach(clazz -> {
+            SimpleCommand annotation = clazz.getAnnotation(SimpleCommand.class);
+            String description = annotation.description();
+            String usage = annotation.usage();
+
+            embed.addField(usage, description, true);
+        });
+
+        event.getChannel().sendMessageEmbeds(embed.build()).queue();
+    }
+}
