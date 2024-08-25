9176cde7b41cca50849e86ead805acf3c604b539
eunhyun05 <eunhyun050812@gmail.com>
2024-08-25 14:12:18 +0900

    feat: Add utility class for Discord emojis
    - Created `DiscordEmojiUtil` class with static constants for commonly used Discord emojis.
    - Centralized emoji codes such as check mark, cross mark, warning, and ticket.
    - Made the class non-instantiable with a private constructor.
    
    This enhancement simplifies the use of emojis across the codebase by providing a single source of truth for emoji codes.

---
+++ api/src/main/java/io/github/eunhyun/eunhyunbot/api/util/DiscordEmojiUtil.java
@@ -0,0 +1,20 @@
+package io.github.eunhyun.eunhyunbot.api.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DiscordEmojiUtil {
+
+    public static final String CHECK_MARK = "<a:check_mark:1276415022498844752>";
+
+    public static final String CROSS_MARK = "<a:cross_mark:1276415059739807744>";
+
+    public static final String WARNING = "<a:warning:1276495432070987797>";
+
+    public static final String FOX_HUH_QUESTION = "<a:foxhuhquestion:1276380174534967357>";
+
+    public static final String TICKET = "<a:ticket:1276750467585540096>";
+
+    public static final String BAN = "<:ban:1276870432552648867>";
+}
