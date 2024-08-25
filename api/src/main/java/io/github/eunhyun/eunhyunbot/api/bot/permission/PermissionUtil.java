2e6683caff9c39e9dbaa27356daf9f8a66adb7a6
eunhyun05 <eunhyun050812@gmail.com>
2024-08-25 14:12:59 +0900

    fix: Enhance PermissionUtil to include ButtonInteractionEvent and use emojis in error messages

--- api/src/main/java/io/github/eunhyun/eunhyunbot/api/bot/permission/PermissionUtil.java
+++ api/src/main/java/io/github/eunhyun/eunhyunbot/api/bot/permission/PermissionUtil.java
@@ -1,6 +1,7 @@
 package io.github.eunhyun.eunhyunbot.api.bot.permission;
 
 import io.github.eunhyun.eunhyunbot.api.factory.EmbedColorFactory;
+import io.github.eunhyun.eunhyunbot.api.util.DiscordEmojiUtil;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,7 +17,7 @@ import org.jetbrains.annotations.NotNull;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PermissionUtil {
 
-    private static final String EMBED_TITLE = "<a:cross_mark:1276415059739807744> 권한 없음 | 오류 <a:cross_mark:1276415059739807744>";
+    private static final String EMBED_TITLE = "%s 권한 없음 | 오류 %s".formatted(DiscordEmojiUtil.CROSS_MARK, DiscordEmojiUtil.CROSS_MARK);
 
     public static boolean checkPermissionsAndSendError(@NotNull MessageReceivedEvent event, @NotNull Member member,
                                                        @NotNull Permission[] requiredPerms, @NotNull String errorMessage) {
@@ -29,6 +31,17 @@ public class PermissionUtil {
     }
 
     public static boolean checkPermissionsAndSendError(@NotNull SlashCommandInteractionEvent event, @NotNull Member member,
+                                                       @NotNull Permission[] requiredPerms, @NotNull String errorMessage) {
+        for (Permission perm : requiredPerms) {
+            if (!member.hasPermission(perm)) {
+                sendErrorMessage(event, errorMessage);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkPermissionsAndSendError(@NotNull ButtonInteractionEvent event, @NotNull Member member,
                                                        @NotNull Permission[] requiredPerms, @NotNull String errorMessage) {
         for (Permission perm : requiredPerms) {
             if (!member.hasPermission(perm)) {
@@ -54,7 +67,15 @@ public class PermissionUtil {
                         .setTitle(EMBED_TITLE)
                         .setDescription(String.format("> **%s**", errorMessage))
                         .build())
-                .setEphemeral(true)
+                .queue();
+    }
+
+    private static void sendErrorMessage(@NotNull ButtonInteractionEvent event, @NotNull String errorMessage) {
+        event.replyEmbeds(new EmbedBuilder()
+                        .setColor(EmbedColorFactory.getEmbedColor(EmbedColorFactory.Type.ERROR))
+                        .setTitle(EMBED_TITLE)
+                        .setDescription(String.format("> **%s**", errorMessage))
+                        .build())
                 .queue();
     }
 }
