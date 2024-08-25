3fc441a0924998f6a8bf04c6208b0112d254f72e
eunhyun05 <eunhyun050812@gmail.com>
2024-08-25 14:14:41 +0900

    feat: add THUMBNAIL_IMAGE_URL constant to EunhyunImageUtil

---
+++ api/src/main/java/io/github/eunhyun/eunhyunbot/api/util/EunhyunImageUtil.java
@@ -0,0 +1,10 @@
+package io.github.eunhyun.eunhyunbot.api.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class EunhyunImageUtil {
+
+    public static final String THUMBNAIL_IMAGE_URL = "https://cdn.discordapp.com/avatars/1270055424300683347/abecf96df04b7ffee74c0f8fe4c20255.webp?size=1024&format=webp&width=0&height=256";
+}
